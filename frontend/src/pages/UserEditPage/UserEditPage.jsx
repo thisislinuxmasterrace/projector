dfbe33880fea713f25fc8c480cf76df965b330a1
Artyom <n89027538933@gmail.com>
2024-10-02 00:23:20 +0300

    Created user edit page

---
+++ frontend/src/pages/UserEditPage/UserEditPage.jsx
@@ -0,0 +1,44 @@
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+import {useEffect, useState} from "react";
+
+const UserEditPage = () => {
+    const [name, setName] = useState("Артём");
+    const [surname, setSurname] = useState("Лукичев");
+    const [email, setEmail] = useState("v@z.zov");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        window.M.updateTextFields();
+    }, []);
+
+    return (
+        <MainLayout>
+            <div className="page-title">Edit profile</div>
+            <form>
+                <div className="row">
+                    <div className="input-field col s6">
+                        <input value={name} disabled={loading} id="first_name" type="text" className="validate"/>
+                        <label htmlFor="first_name">Name</label>
+                    </div>
+                    <div className="input-field col s6">
+                        <input value={surname} disabled={loading} id="last_name" type="text" className="validate"/>
+                        <label htmlFor="last_name">Surname</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s6">
+                        <input value={email} disabled={loading} id="email" type="email" className="validate"/>
+                        <label htmlFor="email">Email</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <button className="btn waves-effect waves-light orange darken-4" type="submit" name="action">Submit
+                        <i className="material-icons right">send</i>
+                    </button>
+                </div>
+            </form>
+        </MainLayout>
+    );
+};
+
+export default UserEditPage;
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,15 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
+import UserEditPage from "./pages/UserEditPage/UserEditPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainPage />,
+    },
+    {
+        path: "/profile",
+        element: <UserEditPage />
     }
 ]);
 
