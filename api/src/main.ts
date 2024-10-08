9f6c75305b91b8a9294d0d0c68f4bfc5b9b5950b
Kirill Belolipetsky <archhaze24@gmail.com>
2024-10-08 12:55:06 +0300

    change dotenv to dotenvx

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
